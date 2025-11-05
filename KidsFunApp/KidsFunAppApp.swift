18de6aa098ae6ec64815b5bb765b45fae56a09ef
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-11-05 11:03:14 -0500

    Initial Commit

---
+++ KidsFunApp/KidsFunAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsFunAppApp.swift
+//  KidsFunApp
+//
+//  Created by Fatom on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct KidsFunAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KidsFunAppTests/KidsFunAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  KidsFunAppTests.swift
+//  KidsFunAppTests
+//
+//  Created by Fatom on 2025-11-05.
+//
+
+import Testing
+@testable import KidsFunApp
+
+struct KidsFunAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
